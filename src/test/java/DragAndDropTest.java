8811e2f1c9c21af67fe22736c38789f79e107806
Sergey <zendmir5@yandex.ru>
2021-02-10 21:28:45 +0300

    refactoring

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,8 +1,8 @@
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
 
 public class DragAndDropTest extends BaseTest {
     SelenideElement blockA = $("#column-a");
@@ -12,13 +12,14 @@ public class DragAndDropTest extends BaseTest {
     void dragAndDrop() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
 
+        actions().clickAndHold(blockA).moveToElement(blockB).release().build().perform();
+        blockA.shouldHave(text("B"));
+        blockB.shouldHave(text("A"));
+
         /*blockA.dragAndDropTo(blockB);
         Selenide.sleep(3000);*/
 
         /*actions().dragAndDrop(blockA, blockB).perform();
-        Selenide.sleep(3000);*/
-
-        /*actions().clickAndHold(blockA).moveToElement(blockB).release().build().perform();
         Selenide.sleep(3000);*/
     }
 }
--- src/test/java/SoftAssertionsPageTest.java
+++ src/test/java/SoftAssertionsPageTest.java
@@ -1,10 +1,10 @@
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.visible;
 
 public class SoftAssertionsPageTest extends BaseTest {
     @Test
@@ -12,6 +12,6 @@ public class SoftAssertionsPageTest extends BaseTest {
         open("https://github.com/selenide/selenide");
         $("span[data-content='Wiki']").click();
         $("#wiki-pages-box").$(byText("SoftAssertions")).click();
-        $(withText("Using JUnit5")).closest("ol").$x(".//following-sibling::div/pre").shouldBe(Condition.visible);
+        $(withText("Using JUnit5")).closest("ol").sibling(0).$("pre").shouldBe(visible);
     }
 }
