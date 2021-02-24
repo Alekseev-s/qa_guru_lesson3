3dc2693e219cd63bea34fcb76fc38952a8041eba
sergey <zendmir5@yandex.ru>
2021-02-24 21:05:21 +0300

    fixed drag and drop

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,8 +1,9 @@
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class DragAndDropTest extends BaseTest {
     SelenideElement blockA = $("#column-a");
@@ -12,14 +13,12 @@ public class DragAndDropTest extends BaseTest {
     void dragAndDrop() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
 
-        actions().clickAndHold(blockA).moveToElement(blockB).release().build().perform();
+        /*actions().clickAndHold(blockA).moveToElement(blockB).release().build().perform();
+        blockA.shouldHave(text("B"));
+        blockB.shouldHave(text("A"));*/
+
+        blockA.dragAndDropTo(blockB);
         blockA.shouldHave(text("B"));
         blockB.shouldHave(text("A"));
-
-        /*blockA.dragAndDropTo(blockB);
-        Selenide.sleep(3000);*/
-
-        /*actions().dragAndDrop(blockA, blockB).perform();
-        Selenide.sleep(3000);*/
     }
 }
